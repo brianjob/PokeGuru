c95424891fa29c9e5194b2279967f63a9664baed
Brian Barton <brianjob@umich.edu>
2016-09-06 06:47:52 -0400

    Initial Commit

---
+++ PokeGuru/PokeGuru.h
@@ -0,0 +1,19 @@
+//
+//  PokeGuru.h
+//  PokeGuru
+//
+//  Created by Brian Barton on 9/6/16.
+//  Copyright © 2016 Brian Barton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PokeGuru.
+FOUNDATION_EXPORT double PokeGuruVersionNumber;
+
+//! Project version string for PokeGuru.
+FOUNDATION_EXPORT const unsigned char PokeGuruVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokeGuru/PublicHeader.h>
+
+
